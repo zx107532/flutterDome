ce8dc22b47f7e613e404072e5ab712571e7e0f5b
NoahZhu <zhuxiaozyyx@yeah.net>
2018-12-17 22:30:33 +0800

    背景渐变，边框

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,18 @@ class MyApp extends StatelessWidget{
         body: Center(
           child: Container(
             child: Text('测试测试测试测试测',style: TextStyle(fontSize: 40.0),),
-            alignment: Alignment.centerLeft,
+            alignment: Alignment.topLeft,
             width: 500.0,
             height: 400.0,
-            color: Colors.lightBlue,
+            // color: Colors.lightBlue,
+            padding: const EdgeInsets.fromLTRB(10.0, 20.0, 0.0, 0.0),
+            margin: const EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 0.0),
+            decoration: new BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
+              ),
+              border: Border.all(width: 2.0, color: Colors.red)
+            ),
           ),
         ),
       ),
