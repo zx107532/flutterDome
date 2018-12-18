95af677ea0c6f17f7dcac13de7fb629cbeb811ee
NoahZhu <zhuxiaozyyx@yeah.net>
2018-12-19 00:53:45 +0800

    横向列表，组件拆分

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,34 @@ class MyApp extends StatelessWidget{
       title: '练习Flutter',
       home: Scaffold(
         appBar: AppBar(title: new Text('测试测试a')),
-        body: new ListView(
-          children: <Widget>[
-            new Image.network('http://seopic.699pic.com/photo/50057/4433.jpg_wh1200.jpg'),
-            new Image.network('https://c.pxhere.com/images/8f/04/15f4e99ba8b071b3ca061e602bda-1443939.jpg!d'),
-            new Image.network('https://c.pxhere.com/photos/e1/fe/pond_brazil_blue_lagoon_water_landscape-1340028.jpg!d'),
-            new Image.network('https://c.pxhere.com/photos/ee/ae/fog_dam_water_cold_winter_s_o_paulo_paraibuna_landscape-1278660.jpg!d'),
-            new Image.network('https://c.pxhere.com/photos/d1/e3/rio_river_pond_lake_water_nature_sunset_landscape-1224328.jpg!d')
-          ],
-        ),
+        body:Center(
+          child: Container(
+            height: 200.0,
+            child: MyList()
+          )
+        )
       ),
+    );
+  }
+}
+
+class MyList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Image.network('http://seopic.699pic.com/photo/50057/4433.jpg_wh1200.jpg'),
+        new Container(
+          width: 180.0,
+          color: Colors.orange,
+        ),
+        new Image.network('https://c.pxhere.com/images/8f/04/15f4e99ba8b071b3ca061e602bda-1443939.jpg!d'),
+        new Image.network('https://c.pxhere.com/photos/e1/fe/pond_brazil_blue_lagoon_water_landscape-1340028.jpg!d'),
+        new Image.network('https://c.pxhere.com/photos/ee/ae/fog_dam_water_cold_winter_s_o_paulo_paraibuna_landscape-1278660.jpg!d'),
+        new Image.network('https://c.pxhere.com/photos/d1/e3/rio_river_pond_lake_water_nature_sunset_landscape-1224328.jpg!d')
+      ],
     );
   }
 }
