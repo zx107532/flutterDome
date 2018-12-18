0545a7489cc18802c0f5c0a18b03fd0b190e3b14
NoahZhu <zhuxiaozyyx@yeah.net>
2018-12-18 22:57:34 +0800

    图标列表

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
+    // TODO: implement build
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: '练习Flutter',
       home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text('Welcome to Flutter'),
-        // ),
-        body: Center(
-          child: Container(
-            child: new Image.network(
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoYQcP4HU0dTriNDBsI7W0GfcJLQcZRpTjzy7PQ8TQnr21qmh_',
-              fit:BoxFit.contain,
-              color: Colors.lightGreen,
-              colorBlendMode: BlendMode.colorBurn,
-              repeat: ImageRepeat.noRepeat,
+        appBar: AppBar(title: new Text('测试测试a')),
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+              leading: new Icon(Icons.airplay),
+              title: new Text('电脑'),
             ),
-            width: 300.0,
-            height: 200.0,
-            color: Colors.lightBlue,
-          ),
+            new ListTile(
+              leading: new Icon(Icons.ac_unit),
+              title: new Text('雪花'),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.ac_unit),
+              title: new Text('雪花'),
+            )
+          ],
         ),
       ),
     );
