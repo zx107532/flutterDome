3fbdf9d62f60b50bbcd5281dce7df485938f040f
NoahZhu <zhuxiaozyyx@yeah.net>
2018-12-19 01:40:19 +0800

    编写动态列表，并将组件拆分，开始熟悉组件传参

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(
+  items:new List<String>.generate(2000, (i) => "Item $i")
+));
 
 class MyApp extends StatelessWidget{
+  final List<String> items;
+  MyApp({Key key, @required this.items}):super(key:key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -10,17 +15,20 @@ class MyApp extends StatelessWidget{
       title: '练习Flutter',
       home: Scaffold(
         appBar: AppBar(title: new Text('测试测试a')),
-        body:Center(
-          child: Container(
-            height: 200.0,
-            child: MyList()
-          )
+        body: new ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          }
         )
       ),
     );
   }
 }
 
+/*普通横向列表*/
 class MyList extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -31,6 +39,7 @@ class MyList extends StatelessWidget{
         new Image.network('http://seopic.699pic.com/photo/50057/4433.jpg_wh1200.jpg'),
         new Container(
           width: 180.0,
+          height: 200.0,
           color: Colors.orange,
         ),
         new Image.network('https://c.pxhere.com/images/8f/04/15f4e99ba8b071b3ca061e602bda-1443939.jpg!d'),
@@ -38,6 +47,25 @@ class MyList extends StatelessWidget{
         new Image.network('https://c.pxhere.com/photos/ee/ae/fog_dam_water_cold_winter_s_o_paulo_paraibuna_landscape-1278660.jpg!d'),
         new Image.network('https://c.pxhere.com/photos/d1/e3/rio_river_pond_lake_water_nature_sunset_landscape-1224328.jpg!d')
       ],
+    );
+  }
+}
+
+/*动态列表*/
+class ListDynamic extends StatelessWidget{
+  final List<String> item;
+  ListDynamic({Key key, @required this.item}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new ListView.builder(
+      itemCount: item.length,
+      itemBuilder: (context, index) {
+        return new ListTile(
+          title: new Text('$item[index]'),
+        );
+      },
     );
   }
 }
