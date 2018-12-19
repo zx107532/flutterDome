abc8f36703b6f19309bfffb70cee6913f35f4d7a
NoahZhu <zhuxiaozyyx@yeah.net>
2018-12-19 14:10:02 +0800

    图片网格列表

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MyApp(items: new List<String>.generate(100, (i) => "Item $i")));
+void main() => runApp(MyApp(
+//    items: new List<String>.generate(100, (i) => "Item $i")
+));
 
 class MyApp extends StatelessWidget {
-  final List<String> items;
-
-  MyApp({Key key, @required this.items}) : super(key: key);
+//  final List<String> items;
+//  MyApp({Key key, @required this.items}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: '练习Flutter',
       home: Scaffold(
         appBar: AppBar(title: new Text('测试测试a')),
-        body: ListDynamic(item: items),
+        body: GridList(),
       ),
     );
   }
@@ -67,6 +67,27 @@ class ListDynamic extends StatelessWidget {
           title: new Text('${item[index]}$index'),
         );
       },
+    );
+  }
+}
+
+/*网格列表*/
+class GridList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.count(
+      padding: const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0),
+      crossAxisCount: 3,
+      crossAxisSpacing: 10.0,
+      children: <Widget>[
+        new Image.network('http://seopic.699pic.com/photo/50057/4433.jpg_wh1200.jpg'),
+        new Image.network('https://c.pxhere.com/images/8f/04/15f4e99ba8b071b3ca061e602bda-1443939.jpg!d'),
+        new Image.network('https://c.pxhere.com/photos/e1/fe/pond_brazil_blue_lagoon_water_landscape-1340028.jpg!d'),
+        new Image.network('https://c.pxhere.com/photos/ee/ae/fog_dam_water_cold_winter_s_o_paulo_paraibuna_landscape-1278660.jpg!d'),
+        new Image.network('https://c.pxhere.com/photos/d1/e3/rio_river_pond_lake_water_nature_sunset_landscape-1224328.jpg!d'),
+        new Image.network('https://c.pxhere.com/images/8f/04/15f4e99ba8b071b3ca061e602bda-1443939.jpg!d')
+      ],
     );
   }
 }
